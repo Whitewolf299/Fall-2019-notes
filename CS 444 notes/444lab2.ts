e5459af9bc90c4284d424c7c067e35dea932da31
Whitewolf299 <sylvanbrander98@gmail.com>
2019-01-30 16:50:07 -0800

    the messed up garbge that I cal notes from 212 and the notes from 444 lab

---
+++ CS 444 notes/444lab2.ts
@@ -0,0 +1,25 @@
+// JavaScript source code
+
+import java.io.*
+import java.util.*
+
+    public class Fib
+    {
+
+        public static void main (String[] args)
+        {
+    integer FIB1 = 0, FIB2 = 1, FIB3 = 1;
+
+    System.out.println("The Fibonacci Series is as follows");
+    system.out.println();
+    System.out.println(FIB1 + " " + FIB2 + " ");
+
+    FIB3 = FIB1 + FIB2;
+    FIB1 = FIB2;
+    FIB2 = FIB3;
+
+    System.out.println(FIB3 + " ");
+
+        }
+        System.out.println();
+    }
---
+++ CS 444 notes/Script1.ts
@@ -0,0 +1,12 @@
+// JavaScript source code
+
+import java.io.*
+import java.util.*
+
+    public class fib
+    {
+        static integer fib1 = 0;
+        static integer fib2 = 1;
+        static integer fib3 = 1;
+
+    }
